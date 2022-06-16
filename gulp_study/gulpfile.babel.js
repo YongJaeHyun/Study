5852aed3dd45235b69f3258e653c786127922cd4
YongjaeHyun <dltjrrbs2020@kumoh.ac.kr>
2022-06-16 22:27:26 +0900

    .nvmrc파일을 통해, 해당 디렉터리로 들어갔을 때 node버전을 자동으로 맞춰줌 babelify 및 여러 gulp 라이브러리들 추가하여 css 경량화,  scss 브라우저 호환성 증가, node문법 commonJS로 치환 등 안정성을 높이고 성능을 개선함

--- gulp_study/gulpfile.babel.js
+++ gulp_study/gulpfile.babel.js
@@ -5,6 +5,10 @@ import ws from "gulp-webserver";
 import image from "gulp-image";
 import gulpSass from "gulp-sass";
 import sass1 from "sass";
+import autoprefixer from "gulp-autoprefixer";
+import miniCSS from "gulp-csso";
+import bro from "gulp-bro";
+import babelify from "babelify";
 
 const sass = gulpSass(sass1);
 
@@ -23,12 +27,36 @@ const routes = {
     src: "src/scss/style.scss",
     dest: "build/css",
   },
+  js: {
+    watch: "src/js/**/*.js",
+    src: "src/js/main.js",
+    dest: "build/js",
+  },
 };
+
+const js = () =>
+  gulp
+    .src(routes.js.src)
+    .pipe(
+      bro({
+        transform: [
+          babelify.configure({ presets: ["@babel/preset-env"] }),
+          ["uglifyify", { global: true }],
+        ],
+      })
+    )
+    .pipe(gulp.dest(routes.js.dest));
 
 const styles = () =>
   gulp
     .src(routes.scss.src)
     .pipe(sass().on("error", sass.logError))
+    .pipe(
+      autoprefixer({
+        overrideBrowserslist: ["last 2 versions"],
+      })
+    )
+    .pipe(miniCSS())
     .pipe(gulp.dest(routes.scss.dest));
 
 const pug = () =>
@@ -36,11 +64,13 @@ const pug = () =>
 
 const clean = () => del(["build/"]);
 
-const webserver = () => gulp.src("build").pipe(ws({ livereload: true }));
+const webserver = () =>
+  gulp.src("build").pipe(ws({ livereload: true, open: true }));
 
 const watch = () => {
   gulp.watch(routes.pug.watch, pug);
   gulp.watch(routes.scss.watch, styles);
+  gulp.watch(routes.js.watch, js);
 };
 
 const img = () =>
@@ -48,7 +78,7 @@ const img = () =>
 
 const prepare = gulp.series([clean, img]);
 
-const assets = gulp.series([pug, styles]);
+const assets = gulp.series([pug, styles, js]);
 
 const live = gulp.parallel([webserver, watch]);
 
